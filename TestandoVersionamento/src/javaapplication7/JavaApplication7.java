7090ae90fbee1749ae8021ffa1529cd5cb5971f2
JoaoPauloDurante <joaopaulojphwa@gmail.com>
2016-11-19 22:00:24 -0200

    Adicionei o metodo dividir

--- TestandoVersionamento/src/javaapplication7/JavaApplication7.java
+++ TestandoVersionamento/src/javaapplication7/JavaApplication7.java
@@ -15,12 +15,21 @@ public class JavaApplication7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
     }
-    public static int somar(int v1,int v2){
-    
+
+    public static int somar(int v1, int v2) {
+
         int soma;
-        soma=v1=v2;
+        soma = v1 = v2;
         return soma;
-    } 
+    }
+
+    public static double dividir(double v1, double v2) {
+
+        double divisao;
+
+        divisao = v1 / v2;
+        return divisao;
+    }
 }
